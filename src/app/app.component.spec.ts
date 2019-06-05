804de6b2268366975552f65a9b78e766de3a0c96
tuandht97 <tuandht97@gmail.com>
2019-06-05 20:02:41 +0700

    update name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'star-admin-angular'`, () => {
+  it(`should have as title 'trit'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('star-admin-angular');
+    expect(app.title).toEqual('trit');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to star-admin-angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to trit!');
   });
 });
